4592e7e06f2e77b506016d1df776b3112bba95d7
phanguy30 <phan.nguyen@mail.utoronto.ca>
2026-03-08 19:50:55 -0700

    Update Dash app to use dynamic port from environment
    Refactor server port configuration to use environment variable.

--- app/dash_app.py
+++ app/dash_app.py
@@ -1,18 +1,23 @@
-from dash import Dash
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 from app.layout import create_layout
 from app.ui_callbacks import register_callbacks
 
 app = Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.CERULEAN],
-    suppress_callback_exceptions=True
+    __name__,
+    external_stylesheets=[dbc.themes.CERULEAN],
+    suppress_callback_exceptions=True
 )
 
 server = app.server
 
+
 app.layout = create_layout()
+
 register_callbacks(app)
 
+import os
+
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=False)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host="0.0.0.0", port=port)
