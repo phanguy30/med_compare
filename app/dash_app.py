6ae533ced68c81d4c111dff24341e4ed598dfc06
phanguy30 <phan.nguyen@mail.utoronto.ca>
2026-03-08 19:25:16 -0700

    Update app.run to use environment variable for port

--- app/dash_app.py
+++ app/dash_app.py
@@ -14,5 +14,8 @@ app.layout = create_layout()
 
 register_callbacks(app)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host="0.0.0.0", port=port)
