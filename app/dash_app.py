b70fbf261c50ed64a540e5ab5b493c1beac0bb54
phan.nguyen <phan.nguyen@mail.utoronto.ca>
2026-03-08 20:01:28 -0700

    fixing typo

--- app/dash_app.py
+++ app/dash_app.py
@@ -1,23 +1,21 @@
-from dash import Dash, html, dcc
+from dash import Dash
 import dash_bootstrap_components as dbc
 from app.layout import create_layout
 from app.ui_callbacks import register_callbacks
+import os
 
 app = Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.CERULEAN],
-    suppress_callback_exceptions=True
+    __name__,
+    external_stylesheets=[dbc.themes.CERULEAN],
+    suppress_callback_exceptions=True
 )
 
 server = app.server
-
 
 app.layout = create_layout()
 
 register_callbacks(app)
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
